edbc68b6492a3927cf2b7d88f0fd9a9eff56bae0
Hajek Jiri <hajekj14@gmail.com>
2018-12-05 16:25:09 +0100

    fix: barrel file
    barrel file

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { LightMapperRunner } from './light-mapper-runner'
+export {
+  MappingRequirement,
+  MappingMetadata,
+  MappingOpts,
+  Mapping
+} from './light-mapper.decorators'
+export { LightMapper } from './light-mapper'
+export { MapperCallBack, IAssocFunction, IAssocAny } from './light-mapper.types'
